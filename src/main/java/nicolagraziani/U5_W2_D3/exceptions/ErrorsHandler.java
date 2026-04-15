0875e00131d922b85f73c86de7e48ea5b9e2a30b
Graziani Nicola <graziani.nicola2000@gmail.com>
2026-04-15 16:53:58 +0200

    add ErrorsHandler

---
+++ src/main/java/nicolagraziani/U5_W2_D3/exceptions/ErrorsHandler.java
@@ -0,0 +1,31 @@
+package nicolagraziani.U5_W2_D3.exceptions;
+
+import nicolagraziani.U5_W2_D3.payloads.ErrorsPayload;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ErrorsHandler {
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST) // 400
+    public ErrorsPayload handleBadRequest(BadRequestException ex) {
+        return new ErrorsPayload(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND) // 404
+    public ErrorsPayload handleNotFoundEx(NotFoundException ex) {
+        return new ErrorsPayload(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR) // 500
+    public ErrorsPayload handleGenericEx(Exception ex) {
+        ex.printStackTrace();
+        return new ErrorsPayload("C'è stato un errore lato server, giuro che lo risolveremo presto!", LocalDateTime.now());
+    }
+}
